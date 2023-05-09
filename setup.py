e3c023a6b93c14f60c1d0a84295f4baa20ebbd88
Jesse Polman <jesse.polman123@gmail.com>
2023-05-09 11:30:16 +0200

    Now the package can be installed locally on our computer

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="rv_curve_jura",
+    version="0.1",
+    author="Jesse Polman",
+    author_email="jesse.polman123@gmail.com",
+    description="Plot RV curves",
+    packages=setuptools.find_packages(),
+    install_requires=["numpy","matplotlib"],
+    classifiers=["Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    ]
+    )
